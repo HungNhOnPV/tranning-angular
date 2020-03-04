0e154057708eb27ff434481bb245e4f8c088d949
HungNhOnPV <anhhungx9propro@gmail.com>
2020-03-04 21:08:42 +0700

    tow way binding

--- lesson02-DataBinding/src/app/app.module.ts
+++ lesson02-DataBinding/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ComponentToViewComponent } from './components/component-to-view/component-to-view.component';
 import { ViewToComponentComponent } from './components/view-to-component/view-to-component.component';
+import { TwoWayBindingComponent } from './components/two-way-binding/two-way-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ComponentToViewComponent,
-    ViewToComponentComponent
+    ViewToComponentComponent,
+    TwoWayBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
