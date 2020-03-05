6307ee80bc67ad692aa66ede0e5dd41912d358a8
HungNhOnPV <anhhungx9propro@gmail.com>
2020-03-05 13:36:09 +0700

    structural directive: *ngFor

--- lesson03-Directive/src/app/component/structural/structural.component.ts
+++ lesson03-Directive/src/app/component/structural/structural.component.ts
@@ -1,24 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-structural',
-  templateUrl: './structural.component.html',
-  styleUrls: ['./structural.component.css']
+  selector: "app-structural",
+  templateUrl: "./structural.component.html",
+  styleUrls: ["./structural.component.css"]
 })
 export class StructuralComponent implements OnInit {
-
   isShow: boolean = false;
-  userName: string = 'Pham Viet Hung';
+  userName: string = "Pham Viet Hung";
   isChecked: boolean = true;
   age: number;
+  names: string[] = ["Ha Noi", "Nam Dinh"];
+  products: any[] = [
+    {
+      id: 1,
+      name: "I phone 6 plus",
+      price: 3000000
+    }
+  ];
 
-  constructor() { }
+  productsFormServers: any[] = [
+    {
+      id: 1,
+      name: "I phone 6 plus",
+      price: 3000000
+    },
+    {
+      id: 2,
+      name: "I phone 7 plus",
+      price: 10000000
+    },
+    {
+      id: 3,
+      name: "I phone 8 plus",
+      price: 11000000
+    },
+    {
+      id: 4,
+      name: "I phone 10 plus",
+      price: 15000000
+    },
+    {
+      id: 5,
+      name: "I phone 11 plus",
+      price: 5000000
+    }
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
   onChange = value => {
     this.isChecked = value;
-  }
+  };
 
+  onFetchData = () => {
+    this.products = this.productsFormServers;
+  };
+
+  myTrackByFunction = (index, item) => {
+    return item.id;
+  };
 }
