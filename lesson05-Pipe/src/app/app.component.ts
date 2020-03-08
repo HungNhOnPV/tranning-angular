f08ae292ec0adde67d951d0a7b3e81187a6188f6
HungNhOnPV <anhhungx9propro@gmail.com>
2020-03-08 15:20:20 +0700

    slice, decimal, percent for pipe

--- lesson05-Pipe/src/app/app.component.ts
+++ lesson05-Pipe/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
   content: string = `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`;
+  title: string = "Learn angular 4";
+  amount: number = 35.142567;
+  result: number = 0.8516;
+  names: string[] = ["abc", "def", "xyz", "123", "456"];
 }
