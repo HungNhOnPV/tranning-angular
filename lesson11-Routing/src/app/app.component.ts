6b8ed9c591fb79177c317d26840bdbaf428ef437
HungNhOnPV <anhhungx9propro@gmail.com>
2020-03-15 11:54:34 +0700

    routing

---
+++ lesson11-Routing/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  title: string = "Routing";
+
+  constructor(public routerService: Router) {}
+
+  navigate = (url: string) => {
+    // this.routerService.navigate([url]);
+    this.routerService.navigateByUrl(`${url}`);
+  };
+}
---
+++ lesson11-Routing/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'index',
+    pathMatch: 'full'
+  },
+  {
+    path: 'index',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    ContactComponent,
+    HomeComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
