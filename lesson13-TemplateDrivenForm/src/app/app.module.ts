4da7fdaa9618ff978aa169fc255874005d3cb372
HungNhOnPV <anhhungx9propro@gmail.com>
2020-03-21 21:40:32 +0700

    data driven form

--- lesson13-TemplateDrivenForm/src/app/app.module.ts
+++ lesson13-TemplateDrivenForm/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { TemplateFormComponent } from './components/template-form/template-form.component';
-
+import { DataDrivenFormComponent } from './components/data-driven-form/data-driven-form.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TemplateFormComponent
+    TemplateFormComponent,
+    DataDrivenFormComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ lesson13-TemplateDrivenForm/src/app/components/data-driven-form/data-driven-form.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Subscription } from "rxjs";
+
+@Component({
+  selector: "app-data-driven-form",
+  templateUrl: "./data-driven-form.component.html",
+  styleUrls: ["./data-driven-form.component.css"]
+})
+export class DataDrivenFormComponent implements OnInit, OnDestroy {
+  frmUser: FormGroup;
+  subscription: Subscription;
+
+  constructor(private _formBuilderService: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+
+  createForm = () => {
+    this.frmUser = this._formBuilderService.group({
+      username: [
+        "",
+        [Validators.required, Validators.minLength(5), Validators.maxLength(20)]
+      ],
+      password: [
+        "",
+        [Validators.required, Validators.minLength(5), Validators.maxLength(20)]
+      ],
+      fullname: [""],
+      email: [
+        "",
+        Validators.pattern(
+          "[a-zA-Z0-9.-_]{1,}@[a-zA-Z.-]{2,}[.]{1}[a-zA-Z]{2,}"
+        )
+      ],
+      phone: [""]
+    });
+
+    this.subscription = this.frmUser.valueChanges.subscribe(data => {
+      console.log(data);
+    });
+  };
+
+  onSubmitForm = () => {
+    console.log(this.frmUser);
+  };
+
+  onResetForm = () => {
+    this.frmUser.reset();
+  };
+}
